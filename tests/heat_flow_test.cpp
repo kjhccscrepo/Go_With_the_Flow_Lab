1651ae751a02e5146f4c232c48a80db4a53b93c6
kjhccscrepo <j_kist1@mail.harpercollege.edu>
2025-10-14 12:02:21 -0500

    add project skeleton

---
+++ tests/heat_flow_test.cpp
@@ -0,0 +1,14 @@
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/benchmark/catch_benchmark.hpp>
+#include <catch2/benchmark/catch_constructor.hpp>
+#include <catch2/generators/catch_generators_range.hpp>
+#include "../src/heat_flow.hpp"
+#include "../src/heat_flow_config.hpp"
+#include "../src/oops.hpp"
+
+TEST_CASE(" mogus ") {
+    REQUIRE(true == true);
+}
+TEST_CASE(" mogus2 ") {
+    REQUIRE(true == false);
+}
